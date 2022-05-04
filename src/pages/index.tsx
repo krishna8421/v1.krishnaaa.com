cdf745a76f82cfd77ae71e3a49d583bec1856daf
Krishna Kumar <71817691+krishna8421@users.noreply.github.com>
2022-05-04 06:15:46 +0530

    Init

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next"
+
+const Index: NextPage = () => (
+  <div className="min-h-screen flex justify-center items-center bg-gray-50">
+    krishnaaa.com
+  </div>
+)
+
+export default Index
