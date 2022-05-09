7311b030697f449830fcb8667bf0c532795ca4e6
Krishna Kumar <71817691+krishna8421@users.noreply.github.com>
2022-05-10 00:05:36 +0530

    Add: Local Fonts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
 // const withPWA = require('next-pwa')
 // const runtimeCaching = require('next-pwa/cache')
+
+/**
+ * @type {import('next').NextConfig}
+ */
 
 const nextConfig = {
     reactStrictMode: true,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        IBMPlexMono: ["IBMPlexMono", ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 }
