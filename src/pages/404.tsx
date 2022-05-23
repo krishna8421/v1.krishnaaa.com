15655600d3a31ceb4a1e5cdb3bbc192088e09a50
Krishna Kumar <71817691+krishna8421@users.noreply.github.com>
2022-05-23 09:34:56 +0530

    Fix: goToHomePage from 404 link

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,22 @@
 import { LayerButton } from "@components/LayerButton";
 import { Main } from "@layouts/Main";
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 
 const NotFound: NextPage = () => {
+  const router = useRouter();
+  const goToHomePage = () => router.push("/");
   return (
     <Main title="Page not found">
       <div className="flex items-center flex-col font-PressStart2P">
         <span className="text-8xl my-12">404</span>
         <h3 className="md:text-2xl text-xl">page on vacation</h3>
-        <LayerButton text="Back to Home" bgClass="my-12" buttonClass="py-3 px-6 " />
+        <LayerButton
+          text="Back to Home"
+          bgClass="my-12"
+          buttonClass="py-3 px-6"
+          onClick={goToHomePage}
+        />
       </div>
     </Main>
   );
