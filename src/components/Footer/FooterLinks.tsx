03c2c301b65fb9392f2bf4a11833515006df4474
Krishna Kumar <71817691+krishna8421@users.noreply.github.com>
2022-05-25 08:45:29 +0530

    Fix: footer home link fix

--- src/components/Footer/FooterLinks.tsx
+++ src/components/Footer/FooterLinks.tsx
@@ -5,7 +5,7 @@ export const FooterLinks = () => {
   return (
     <div className="flex gap-32 font-bold text-gray-300 text-sm justify-center">
       <div className="flex flex-col gap-8 items-center">
-        <FooterLink url="/home" name="home" />
+        <FooterLink url="/" name="home" />
         <FooterLink url="/blogs" name="blogs" />
         <FooterLink url="/contact" name="contact" />
       </div>
